38f4fe7f5713f02ea6a5d32e2d5fdd7f5c4c6d1b
audriusxp <palm@inbox.lt>
2022-06-28 12:52:06 +0300

    sukurta funkcija

---
+++ src/uzduotis2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class uzduotis2 {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        Scanner skaitytuvas = new Scanner(System.in);
+        System.out.println("iveskite skaiciu X : ");
+        x = skaitytuvas.nextInt();
+        System.out.println("x = " + x);
+        Scanner skaitytuvas1 = new Scanner(System.in);
+        System.out.println("iveskite skaiciu Y : ");
+        y = skaitytuvas1.nextInt();
+        System.out.println("y = " + y);
+        System.out.println(" arLyginis(int x, int y)= " + arLyginis(x,y));
+
+    }
+
+    public static int arLyginis(int x, int y) {
+        if (x % 2 >=0) {
+            return x*y;
+
+        else if (y > 100)
+            return x-y;
+
+        if (y<100){
+            return y-x;
+
+        else { y=100;
+            return 0;
+
+
+    }
+}
+
+
+
+
+
+
+
+
